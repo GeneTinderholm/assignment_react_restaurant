67297b1da43cc8ad77067c680ee1e8fde7042876
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-18 12:22:57 -0600

    timer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import barNav from './components/navbar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <barNav />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+const barNav = (props) => {
+  return ( <div>
+  <ul>
+  <li>Reservation</li>
+  <li>Welcome</li>
+  <li>Menu</li>
+  </ul>
+  </div> 
+) }
+
+export default barNav
