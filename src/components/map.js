a4c36f17e4b66d3e38d133417cd546d8eb0f325b
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-18 17:44:43 -0600

    remove api key

--- src/components/map.js
+++ src/components/map.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import {withGoogleMap, GoogleMap, Marker} from "react-google-maps";
-//
-// const MapWithAMarker = withGoogleMap(props => (
-//   <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
-//     <Marker position={{lat: -34.397, lng: 150.644}} />
-//   </GoogleMap>
-// ));
-//
-// const Map = props => {
-//   return (
-//     <MapWithAMarker
-//       containerElement={<div style={{height: `400px`}} />}
-//       mapElement={<div style={{height: `100%`}} />}
-//     />
-//   );
-// };
-
 import React, {Component} from 'react';
 // import {GoogleApiWrapper}from 'google-map-react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
@@ -41,55 +23,3 @@ render() {
 export default GoogleApiWrapper({
   apiKey: (ApiKey)
 })(MapContainer)
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// const AnyReactComponent = ({text}) => (
-//   <div
-//     style={{
-//       position: 'relative',
-//       color: 'white',
-//       background: 'red',
-//       height: 40,
-//       width: 60,
-//       top: -20,
-//       left: -30,
-//     }}>
-//     {text}
-//   </div>
-// );
-//
-// class SimpleMap extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 59.95,
-//       lng: 30.33,
-//     },
-//     zoom: 11,
-//   };
-//
-//   // AIzaSyDsJUw8AMOAQ5EyUsGGDG9U7edCeL4JBeI
-//
-//   render() {
-//     return (
-//       <div className="containter">
-//         <GoogleMapReact
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}>
-//           <GoogleMap
-//             bootstrapURLKeys={{
-//               key: 'AIzaSyDsJUw8AMOAQ5EyUsGGDG9U7edCeL4JBeI',
-//               language: 'en',
-//             }}
-//           />
-//           <AnyReactComponent
-//             lat={59.955413}
-//             lng={30.337844}
-//             text={'Kreyser Avrora'}
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-// }
-
-// export default SimpleMap;
